e688f0f961cc5885ee5c9c63c897f2a9c907aa1e
subirxd69 <sudiptamondal787@gmail.com>
2024-05-01 00:07:35 +0530

    first commit

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://sudiptamondal787:ecommerce@cluster0.zov50zf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
---
+++ src/utils/index.js
@@ -0,0 +1,240 @@
+export const navOptions = [
+  {
+    id: "home",
+    label: "Home",
+    path: "/",
+  },
+  {
+    id: "listing",
+    label: "All Products",
+    path: "/product/listing/all-products",
+  },
+  {
+    id: "listingMen",
+    label: "Men",
+    path: "/product/listing/men",
+  },
+  {
+    id: "listingWomen",
+    label: "Women",
+    path: "/product/listing/women",
+  },
+  {
+    id: "listingKids",
+    label: "kids",
+    path: "/product/listing/kids",
+  },
+];
+
+export const adminNavOptions = [
+  {
+    id: "adminListing",
+    label: "Manage All Products",
+    path: "/admin-view/all-products",
+  },
+  {
+    id: "adminNewProduct",
+    label: "Add New Product",
+    path: "/admin-view/add-product",
+  },
+];
+
+export const registrationFormControls = [
+  {
+    id: "name",
+    type: "text",
+    placeholder: "Enter your name",
+    label: "Name",
+    componentType: "input",
+  },
+  {
+    id: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+    componentType: "input",
+  },
+  {
+    id: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    label: "Password",
+    componentType: "input",
+  },
+  {
+    id: "role",
+    type: "",
+    placeholder: "",
+    label: "Role",
+    componentType: "select",
+    options: [
+      {
+        id: "admin",
+        label: "Admin",
+      },
+      {
+        id: "customer",
+        label: "customer",
+      },
+    ],
+  },
+];
+
+export const loginFormControls = [
+  {
+    id: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+    componentType: "input",
+  },
+  {
+    id: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    label: "Password",
+    componentType: "input",
+  },
+];
+
+export const ProductformControls = [
+  {
+    id: "name",
+    type: "text",
+    placeholder: "Enter name",
+    label: "Name",
+    componentType: "input",
+  },
+  {
+    id: "price",
+    type: "number",
+    placeholder: "Enter price",
+    label: "Price",
+    componentType: "input",
+  },
+  {
+    id: "description",
+    type: "text",
+    placeholder: "Enter description",
+    label: "Description",
+    componentType: "input",
+  },
+  {
+    id: "category",
+    type: "",
+    placeholder: "",
+    label: "Category",
+    componentType: "select",
+    options: [
+      {
+        id: "men",
+        label: "Men",
+      },
+      {
+        id: "women",
+        label: "Women",
+      },
+      {
+        id: "kids",
+        label: "Kids",
+      },
+    ],
+  },
+  {
+    id: "deliveryInfo",
+    type: "text",
+    placeholder: "Enter deliveryInfo",
+    label: "Delivery Info",
+    componentType: "input",
+  },
+  {
+    id: "onSale",
+    type: "",
+    placeholder: "",
+    label: "On Sale",
+    componentType: "select",
+    options: [
+      {
+        id: "yes",
+        label: "Yes",
+      },
+      {
+        id: "no",
+        label: "No",
+      },
+    ],
+  },
+  {
+    id: "priceDrop",
+    type: "number",
+    placeholder: "Enter Price Drop",
+    label: "Price Drop",
+    componentType: "input",
+  },
+];
+
+export const AvailableSizes = [
+  {
+    id: "s",
+    label: "S",
+  },
+  {
+    id: "m",
+    label: "M",
+  },
+  {
+    id: "l",
+    label: "L",
+  },
+];
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD529_mIIFDYSA8kI6v0wYS7hv4KqARTFs",
+  authDomain: "project-24414.firebaseapp.com",
+  projectId: "project-24414",
+  storageBucket: "project-24414.appspot.com",
+  messagingSenderId: "124856931838",
+  appId: "1:124856931838:web:1bdd04140c95799d32dcaf",
+  measurementId: "G-FYDM75JCCJ"
+};
+
+export const firebaseStroageURL =
+  "gs://project-24414.appspot.com";
+
+export const addNewAddressFormControls = [
+  {
+    id: "fullName",
+    type: "input",
+    placeholder: "Enter your full name",
+    label: "Full Name",
+    componentType: "input",
+  },
+  {
+    id: "address",
+    type: "input",
+    placeholder: "Enter your full address",
+    label: "Address",
+    componentType: "input",
+  },
+  {
+    id: "city",
+    type: "input",
+    placeholder: "Enter your city",
+    label: "City",
+    componentType: "input",
+  },
+  {
+    id: "country",
+    type: "input",
+    placeholder: "Enter your country",
+    label: "Country",
+    componentType: "input",
+  },
+  {
+    id: "postalCode",
+    type: "input",
+    placeholder: "Enter your postal code",
+    label: "Postal Code",
+    componentType: "input",
+  },
+];
